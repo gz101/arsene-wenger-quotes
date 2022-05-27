d274b98defdd8293202a095ebe7bf0b98f35eb05
Gabriel Chiong <gabrielzhenchiong@gmail.com>
2022-05-28 08:07:38 +1000

    Init project from Spring Initializr. Closes #1

---
+++ src/main/java/me/gabrielchiong/quoteswebapp/QuotesWebappApplication.java
@@ -0,0 +1,13 @@
+package me.gabrielchiong.quoteswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotesWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuotesWebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/gabrielchiong/quoteswebapp/QuotesWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gabrielchiong.quoteswebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotesWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
